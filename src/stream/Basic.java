dc41f1ef92bef34c6afcc36179601fcddca10fcd
panukale <pranaykale06@gmail.com>
2026-07-20 22:55:04 +0530

    Added stream api basic examples.

--- src/stream/Basic.java
+++ src/stream/Basic.java
@@ -5,7 +5,10 @@
 package stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -68,11 +71,55 @@ public class Basic {
 
     }
 
+    public static void evenList() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println("Even List :" + list);
+
+        Map<String, List<Integer>> map = numbers.stream().collect(Collectors.groupingBy(res -> res % 2 == 0 ? "Even" : "Odd"));
+        System.out.println("Advence :" + map);
+    }
+
+    public static void findMaxFromList() {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        Optional<Integer> num = numbers.stream().distinct().sorted(Comparator.reverseOrder()).findFirst();
+        Optional<Integer> max = numbers.stream().max(Integer::compareTo);
+        max.ifPresent(System.out::println);
+        System.out.println("Res :" + num);
+    }
+
+    public static void sortedList() {
+        List<Integer> numbers = Arrays.asList(3, 5, 1, 4, 2);
+        List<Integer> sortedListReversed = numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<Integer> sortedList = numbers.stream().sorted().collect(Collectors.toList());
+        System.out.println(sortedList + " " + sortedListReversed);
+    }
+
+//4. Count Strings with Specific Prefix
+    public static void countPrefixString() {
+        List<String> names = Arrays.asList("Alice", "Bob", "Annie", "Alex", "Charlie");
+        System.out.println(names.stream().filter(x -> x.startsWith("A")).count());
+    }
+
+//5. Find First Non-Repeated Character in a String
+    public static void nonRepeatedChar() {
+        String input = "swiss";
+        Optional<Character> nonRe = input.chars().mapToObj(c -> (char) c).filter(x -> input.indexOf(x) == input.lastIndexOf(x)).findFirst();
+        System.out.println("Non-Rep char:" + nonRe);
+    }
+
     public static void main(String[] args) {
 //        convertToUppercase();
 //        colletToCollection();
 //        countNumbers();
 //        sumOfEvenNumbers();
-        makeStreamFromArray();
+//        makeStreamFromArray();
+//        evenList();
+//        findMaxFromList();
+//        sortedList();
+//        countPrefixString();
+        nonRepeatedChar();
     }
 }
